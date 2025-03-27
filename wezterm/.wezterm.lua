4f6c81e938effd2573dd31bf6482220c3dd07017
Murilo Ramalho <murilo.ramalho@proton.me>
2025-03-27 12:10:38 -0300

    removido keybinds

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -27,19 +27,6 @@ return {
     -- Cursor
     default_cursor_style = "SteadyBlock",
 
-    -- Keybinds focadas no uso do ALT
-    keys = {
-        -- Abas
-        { key = "t", mods = "ALT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-        { key = "w", mods = "ALT", action = wezterm.action.CloseCurrentTab { confirm = true } },
-
-        -- Navegação entre painéis
-        { key = "h", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
-        { key = "l", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
-        { key = "k", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
-        { key = "j", mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
-    },
-
     -- Histórico de Rolagem Ampliado
     scrollback_lines = 5000,
 }
